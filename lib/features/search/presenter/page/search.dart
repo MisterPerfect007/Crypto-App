7f34db99bd0c53d14211a30b3c747faaa3dfa410
MisterPerfect007 <leneodepart@gmail.com>
2022-09-13 11:59:03 +0000

    Building search page

--- lib/features/search/presenter/page/search.dart
+++ lib/features/search/presenter/page/search.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:crypto_trends/injection_container.dart' as di;
 
 import '../../../../ui/colors/colors.dart';
 import '../../../../ui/icons/svg-icons.dart';
+import '../../../coinList/presenter/bloc/coin_list_bloc.dart';
+import '../../../home/presenter/bloc/trending_coin/trending_coin_bloc.dart';
 
 class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
@@ -11,77 +15,96 @@ class Search extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     double width = MediaQuery.of(context).size.width;
     double sidePadding = width / 25;
-    return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size(
-            size.width,
-            50,
-          ),
-          child: SafeArea(
-            child: Container(
-                height: 50,
-                padding: EdgeInsets.only(left: sidePadding, right: sidePadding),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      InkWell(
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Container(
-                              padding: const EdgeInsets.all(10),
-                              child: const SvgIcon(icon: SvgIcons.arrowLeft))),
-                      const SizedBox(width: 10),
-                      Expanded(
-                          child: Container(
-                        alignment: Alignment.center,
-                        height: 35,
-                        padding: const EdgeInsets.only(
-                          left: 5,
-                          right: 5,
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => di.sl<CoinListBloc>()),
+        ],
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size(
+              size.width,
+              50,
+            ),
+            child: SafeArea(
+              child: Container(
+                  height: 50,
+                  color: Colors.white,
+                  padding: EdgeInsets.only(left: 0, right: sidePadding),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                              splashColor: Colors.grey,
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Container(
+                                  padding: const EdgeInsets.all(10),
+                                  child:
+                                      const SvgIcon(icon: SvgIcons.arrowLeft))),
                         ),
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade200,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(30))),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const SvgIcon(
-                                icon: SvgIcons.search,
-                                color: Color.fromARGB(188, 0, 0, 0)),
-                            const SizedBox(width: 10),
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                    child: TextFormField(
-                                      autofocus: true,
-                                      cursorColor: AppColors.mainGreen,
-                                      decoration: const InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "Search coin...",
-                                        hintStyle: TextStyle(
-                                            fontStyle: FontStyle.italic,
-                                            fontSize: 13),
-                                      ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                            child: Container(
+                          alignment: Alignment.center,
+                          height: 30,
+                          padding: const EdgeInsets.only(
+                            left: 5,
+                            right: 5,
+                            // top:5,
+                            // bottom: 5
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(30))),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const SvgIcon(
+                                  icon: SvgIcons.search,
+                                  size: 15,
+                                  color: Color.fromARGB(188, 0, 0, 0)),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: TextFormField(
+                                    cursorWidth: 1.0,
+                                    autofocus: true,
+                                    cursorColor: AppColors.mainGreen,
+                                    decoration: const InputDecoration(
+                                      isDense: true,
+                                      border: InputBorder.none,
+                                      hintText: "Search coin...",
+                                      hintStyle: TextStyle(
+                                          fontStyle: FontStyle.italic,
+                                          fontSize: 10),
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  )
-                                ],
+                                    style: const TextStyle(fontSize: 12)),
                               ),
-                            ),
-                          ],
-                        ),
-                      ))
-                    ])),
+                            ],
+                          ),
+                        ))
+                      ])),
+            ),
           ),
-        ),
-        body: Container(
-          color: Colors.red,
+          body: Container(
+              color: Colors.white,
+              child: BlocBuilder<CoinListBloc, CoinListState>(
+                builder: (context, state) {
+                  print(state);
+                  if (state is CoinListLoaded) {
+                    return Container(
+                      color: Colors.greenAccent,
+                      width: 200,
+                      height: 200,
+                    );
+                  }
+                  return Container();
+                },
+              )),
         ));
   }
 }
